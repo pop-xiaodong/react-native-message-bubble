9755a46d6170a01d259b48e77b049818872ea923
xiaodong <xuxiaodong@tcxy.com.cn>
2016-08-17 13:30:37 +0800

    Support android

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # react-native-message-bubble
-Chat(message) bubble UI, for now just ios.
+Chat(message) bubble UI, both ios and android.
 
 ## Demo
-![image](https://github.com/pop-xiaodong/react-native-message-bubble/blob/master/example/example.png)
+![image](https://github.com/pop-xiaodong/react-native-message-bubble/blob/master/example/example_ios.png)
+![image](https://github.com/pop-xiaodong/react-native-message-bubble/blob/master/example/example_android.jpeg)
 
 ## Installation
 install react-native-message-bubble:
@@ -11,30 +12,68 @@ install react-native-message-bubble:
 npm install react-native-message-bubble --save
 ```
 
+### Android
+```gradle
+// file: android/settings.gradle
+...
+
+include ':react-native-message-bubble'
+project(':react-native-message-bubble').projectDir = new File(settingsDir, '../node_modules/react-native-message-bubble/android')
+```
+```gradle
+// file: android/app/build.gradle
+...
+
+dependencies {
+...
+compile project(':react-native-message-bubble')
+}
+```
+
+```java
+// file: MainActivity.java
+...
+
+import com.bubblemessage.BubbleMessagePackage;
+
+public class MainActivity extends ReactActivity {
+
+@Override
+protected List<ReactPackage> getPackages() {
+return Arrays.<ReactPackage>asList(
+new MainReactPackage(),
+new BubbleMessagePackage()
+);
+}
+...
+}
+
+```
+
 ## Usage
 
 ```js
 var BubbleText = require('react-native-message-bubble');
 
 render() {
-  return (
-    <ListView
-      dataSource={this.state.dataSource}
-      renderRow={this._renderRow}
-      />
-  )
+return (
+<ListView
+dataSource={this.state.dataSource}
+renderRow={this._renderRow}
+/>
+)
 },
 
 _renderRow(rowData, sectionID, rowID) {
-  var messageType = rowData.userType != 'robot';
-  //messageType: if true bubble is on the right, else on left
-  return (
-    <BubbleText key={rowID} messages={rowData.messages} messageType={messageType}/>
-  )
+var messageType = rowData.userType != 'robot';
+//messageType: if true bubble is on the right, else on left
+return (
+<BubbleText key={rowID} messages={rowData.messages} messageType={messageType}/>
+)
 },
 ```
 
 ## Features
-- Android
-- Image and audio
-- CopyText
+- [x] Android
+- [ ] Image and audio
+- [ ] CopyText
--- example/index.android.js
+++ example/index.android.js
@@ -1,53 +1,149 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * react-native-message-bubble
+ * https://github.com/pop-xiaodong/react-native-message-bubble
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
+var React = require('react');
+var {
   StyleSheet,
+  View,
+  Image,
   Text,
-  View
-} from 'react-native';
+  AppRegistry,
+  Keyboard,
+  ListView,
+} = require('react-native');
 
-class example extends Component {
+var InvertibleScrollView = require('react-native-invertible-scroll-view');
+var BubbleText = require('react-native-message-bubble');
+var TextInputView = require('./textInputView');
+
+var Example = React.createClass({
+
+  getInitialState() {
+    var messages = [
+      {
+        messages: '你好~',
+        userType: 'robot',
+      }
+    ];
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    var rowID = messages.map((value, index) => index).reverse();
+    return {
+      dataValue: messages,
+      dataSource: ds.cloneWithRows(messages, rowID),
+      textInputPosition: 0,
+    };
+  },
+
+  componentDidMount() {
+    this._listeners = [
+      Keyboard.addListener('keyboardWillShow', this.onKeyboardWillShow),
+      Keyboard.addListener('keyboardWillHide', this.onKeyboardWillHide),
+    ];
+    setTimeout(() => {
+      this.sendMessage('Can I help you?', 'robot');
+    }, 2000);
+  },
+
+  componentWillUnmount(){
+    this._listeners.map((f) => f.remove());
+    this._listeners = undefined;
+  },
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <ListView
+          style={styles.listView}
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow}
+          renderScrollComponent={props => <InvertibleScrollView {...props} inverted />}
+          enableEmptySections={true}
+          pageSize={8}
+          initialListSize={8}
+          keyboardDismissMode='on-drag'
+          />
+        <View style={{marginBottom: this.state.textInputPosition}}>
+          <TextInputView sendMessage={(textValue, userType) => this.sendMessage(textValue, userType)} />
+        </View>
       </View>
-    );
-  }
-}
+    )
+  },
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  _renderRow(rowData, sectionID, rowID) {
+    var messageType = rowData.userType != 'robot';
+    return (
+      <BubbleText key={rowID} messages={rowData.messages} messageType={messageType}/>
+    )
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  sendMessage(textValue, userType) {
+    var {dataValue, dataSource} = this.state;
+    if (!textValue) {
+      textValue=' ';
+    }
+    var value = {
+      messages: textValue,
+      userType: userType,
+    };
+    dataValue.push(value);
+    var rowID = dataValue.map((value, index) => index).reverse();
+    this.setState({
+      dataValue: dataValue,
+      dataSource: this.state.dataSource.cloneWithRows(dataValue, rowID),
+    });
+    if (userType != 'robot') {
+      setTimeout(() => {
+        this.sendMessage('Confirm', 'robot');
+      }, 2000);
+    }
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  onKeyboardWillShow(ev) {
+    if (ev.endCoordinates) {
+      this.setState({textInputPosition: ev.endCoordinates.height});
+    }
   },
+
+  onKeyboardWillHide(ev) {
+    if (ev.startCoordinates) {
+      this.setState({textInputPosition: 0});
+    }
+  }
 });
 
-AppRegistry.registerComponent('example', () => example);
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lemonchiffon',
+  },
+  listView: {
+    flex:1,
+    marginTop: 20,
+  },
+  bottomView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderTopWidth: 0.5,
+    borderTopColor: '#d3d3d3',
+    height: 44,
+    backgroundColor: '#fafafa',
+  },
+  textInput: {
+    flex: 1,
+    alignSelf: 'center',
+    marginHorizontal: 10,
+    height: 34,
+    borderRadius: 5,
+    borderWidth: 0.5,
+    borderColor: '#d3d3d3',
+    backgroundColor: 'white',
+  },
+  sendText: {
+    alignSelf: 'center',
+    marginRight: 10,
+  }
+});
+
+AppRegistry.registerComponent('example', () => Example);
